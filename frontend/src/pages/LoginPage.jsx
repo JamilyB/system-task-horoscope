44a46c2ab405557a10c7e8846a96890d9fa8f279
Jamily B <jamilybatista09@gmail.com>
2025-06-09 13:27:28 -0300

    modifica rotas

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = () => {
 
 const handleLogin = async (data) => {
   try {
-    const response = await fetch('http://localhost:8080/login', {
+    const response = await fetch('https://system-task-horoscope-backend.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
